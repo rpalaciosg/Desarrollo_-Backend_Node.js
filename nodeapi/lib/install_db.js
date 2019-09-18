5b342b3d5eb3d62ccae6c58bf09d787ed76e3acd
Richard Palacios G <apalaciosg91@gmail.com>
2019-09-18 14:58:20 -0500

    script borrar y cargar agentes

--- nodeapi/lib/install_db.js
+++ nodeapi/lib/install_db.js
@@ -2,8 +2,8 @@
 
 const fs = require('fs');
 const fsPromises = fs.promises;
-
-const conn = require('')
+const conn = require('./connectMongoose');
+const Agente = require('../models/Agente');
 
 const file = '../data/anuncios.json';
 
@@ -11,22 +11,50 @@ const file = '../data/anuncios.json';
 // const anuncios = fs.readFileSync(file, 'utf8');
 // console.log(anuncios);
 
-async function readInsert(filename) {
-    let filehandle = null;
+
+// async function readInsert(filename) {
+//     let filehandle = null;
+//     try {
+//         filehandle = await fsPromises.open(filename);
+//         const data = JSON.parse(await fsPromises.readFile(filename, 'utf-8'));
+//         console.log(data);    
+//     } catch(err) {
+//         console.log('Error', err);   
+//         return; 
+//     } finally {
+//         if (filehandle) {
+//             await filehandle.close();
+//             console.log('Cierro archivo');
+//         }
+//     }
+// }
+
+const data = JSON.parse(fs.readFileSync(file, 'utf-8'));
+async function cargarAgentes() { 
     try {
-        filehandle = await fsPromises.open(filename);
-        const data = await fsPromises.readFile(filename, 'utf8');
-        console.log(data);    
-    } catch(err) {
-        console.log('Error', err);   
-        return; 
-    } finally {
-        if (filehandle) {
-            await filehandle.close();
-            console.log('Cierro archivo');
-        }
+       await Agente.insertMany(data);
+       console.log('Datos cargados.!');
+       process.exit();
+    } catch (err) {
+        console.log('Error al cargar datos', e);
+        process.exit();
     }
 }
 
 console.log('\n * Inicio * \n');
-readInsert(file);
+
+// gestionar eventos de conexión
+conn.on('error', err => {
+    console.log('Error de conexión', err);
+    process.exit(1); // podría hacerme un catálogo de códigos de error.
+});
+
+conn.once('open', async () => {
+    console.log('Conectado a MongoDB en ', mongoose.connection.name);
+    console.log('Limpiar Base de datos..!');
+    const resDel = await Agente.deleteMany();
+    console.log('Base de datos borrada!');
+
+    console.log('Cargando agentes.json!');
+    cargarAgentes();
+});
